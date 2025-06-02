244a35a870294caade3edb92141d19a3a4d8887b
Daniel Trnka <daniel.trnka@gmail.com>
2025-06-02 23:50:21 +0200

    install.sh to install dkms sllin

---
+++ install_driver.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+
+if [ ! -d "linux-lin" ]; then
+	git clone https://github.com/lin-bus/linux-lin.git
+fi
+git -C linux-lin checkout 690f3b868273b146c722364a5377c4b70c85e8fa
+source dkms.conf
+sudo cp -r dkms.conf linux-lin/sllin/ "/usr/src/${PACKAGE_NAME}-${PACKAGE_VERSION}"
+
+sudo dkms remove sllin/0.0.20210831 --all || true
+
+sudo dkms add -m "$PACKAGE_NAME" -v "$PACKAGE_VERSION"
+sudo dkms build -m "$PACKAGE_NAME" -v "$PACKAGE_VERSION"
+sudo dkms install -m "$PACKAGE_NAME" -v "$PACKAGE_VERSION"
