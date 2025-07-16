f5f816f8f6626697c300479ab86972437077705e
Daniel Trnka <daniel.trnka@gmail.com>
2025-07-16 13:14:57 +0200

    fix

--- install_driver.sh
+++ install_driver.sh
@@ -6,9 +6,9 @@ if [ ! -d "linux-lin" ]; then
 fi
 git -C linux-lin checkout 690f3b868273b146c722364a5377c4b70c85e8fa
 source dkms.conf
-sudo cp -r dkms.conf linux-lin/sllin/ "/usr/src/${PACKAGE_NAME}-${PACKAGE_VERSION}"
+sudo cp -r dkms.conf linux-lin/sllin/* "/usr/src/${PACKAGE_NAME}-${PACKAGE_VERSION}"
 
-sudo dkms remove sllin/0.0.20210831 --all || true
+sudo dkms remove sllin/"$PACKAGE_VERSION" --all || true
 
 sudo dkms add -m "$PACKAGE_NAME" -v "$PACKAGE_VERSION"
 sudo dkms build -m "$PACKAGE_NAME" -v "$PACKAGE_VERSION"
